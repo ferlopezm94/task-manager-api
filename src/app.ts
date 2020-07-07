8f22f1a818be21b5d5246b67d66736eafe57f870
Fernando López <ferlopezm94@gmail.com>
2020-07-07 16:53:06 -0500

    feat(api): add endpoint to delete a task

--- src/app.ts
+++ src/app.ts
@@ -164,4 +164,19 @@ app.patch('/tasks/:id', async (req, res) => {
   }
 });
 
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await TaskModel.findByIdAndDelete(req.params.id);
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
+    console.log('error :>> ', error);
+    res.status(500).send();
+  }
+});
+
 app.listen(port, () => console.log(`Server is up and running on port ${port}`));
