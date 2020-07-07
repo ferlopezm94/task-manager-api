883d40b29378e77d4910a7abf1e47e40e34cb9fe
Fernando López <ferlopezm94@gmail.com>
2020-07-07 12:38:25 -0500

    feat(api): add endpoint to update a task

--- src/app.ts
+++ src/app.ts
@@ -122,4 +122,31 @@ app.get('/tasks/:id', async (req, res) => {
   }
 });
 
+app.patch('/tasks/:id', async (req, res) => {
+  const allowedUpdates = ['description', 'completed'];
+  const updates = Object.keys(req.body);
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid to perform the updates sent.' });
+  }
+
+  try {
+    const _id = req.params.id;
+    const task = await TaskModel.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
+    console.log('error :>> ', error);
+    res.status(400).send(error);
+  }
+});
+
 app.listen(port, () => console.log(`Server is up and running on port ${port}`));
