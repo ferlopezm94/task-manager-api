db68c1a417e5dd1ad60eb3d900c106858f618b9e
Fernando López <ferlopezm94@gmail.com>
2020-07-07 11:28:37 -0500

    feat(api): add endpoints to create and read tasks

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import './db/mongoose';
 import { UserModel } from './models/user';
+import { TaskModel } from './models/task';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -47,6 +48,47 @@ app.get('/users/:id', async (req, res) => {
     }
 
     res.send(user);
+  } catch (error) {
+    console.log('error :>> ', error);
+    res.status(500).send();
+  }
+});
+
+// Tasks
+
+app.post('/tasks', async (req, res) => {
+  const task = new TaskModel(req.body);
+
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    console.log('error :>> ', error);
+    res.status(400).send(error);
+  }
+});
+
+app.get('/tasks', async (_req, res) => {
+  try {
+    const tasks = await TaskModel.find({});
+    res.send(tasks);
+  } catch (error) {
+    console.log('error :>> ', error);
+    res.status(500).send();
+  }
+});
+
+app.get('/tasks/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await TaskModel.findById(_id);
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
   } catch (error) {
     console.log('error :>> ', error);
     res.status(500).send();
